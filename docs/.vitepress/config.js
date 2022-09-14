176478c5e1ab6b78b750ed61df7e983a72cc4789
zhangshuai <1016868503@qq.com>
2022-09-14 20:58:33 +0800

    搭建

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  title: "Hello VitePress",
+  description: "Just playing around.",
+  themeConfig: {
+    siteTitle: "前端经验",
+    logo: "/logo.jpg",
+
+    //顶部
+    nav: [
+      { text: "面经", link: "/articles/index" },
+      { text: "算法", link: "/guide/test" },
+      { text: "项目", link: "https://gitee.com/geeksdidi" },
+      {
+        text: "分类",
+        items: [
+          { text: "前端", link: "/item-1" },
+          { text: "移动端", link: "/item-2" },
+          { text: "客户端", link: "/item-3" },
+        ],
+      },
+    ],
+
+    //侧边栏
+    sidebar: {
+      "/articles/": [
+        {
+          text: "测试标签",
+          collapsible: true,
+          collapsed: true,
+          items: [
+            {
+              text: "测试",
+              link: "/articles/index",
+            },
+          ],
+        },
+      ],
+    },
+  },
+
+  //社交
+  socialLinks: [{ icon: "github", link: "https://gitee.com/geeksdidi" }],
+};
---
+++ docs/articles/index.md
@@ -0,0 +1,4 @@
+---
+title: test
+---
+hahah12121
