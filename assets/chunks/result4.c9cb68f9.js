264f08b5987a8e8b39da8017fc5e54c57b3e94ce
zhangshuai <1016868503@qq.com>
2023-02-08 14:55:19 +0800

    'deploy'

---
+++ assets/chunks/result4.c9cb68f9.js
@@ -0,0 +1,1 @@
+import{_ as t,r as s,o as r,c as o,a as c}from"../app.d370b0d0.js";const n={};function l(a,i){const e=s("sb-result");return r(),o("div",null,[c(e,{title:"This is title",subtitle:"This is subtitle",status:"error"})])}const u=t(n,[["render",l]]);export{u as default};
